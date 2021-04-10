4d53671d3e83f4fcd3bf901955ca74627bcececf
Steve <heystevegray@gmail.com>
2021-04-10 19:03:40 -0400

    prettier fun

--- pages/api/upload.ts
+++ pages/api/upload.ts
@@ -4,21 +4,21 @@ import { NextApiRequest, NextApiResponse } from 'next';
 import { inputFile, lucidToPrisma, destinationFolder } from '../../utils';
 
 const upload = multer({
-  storage: multer.diskStorage({
-    destination: destinationFolder,
-    filename: (req, file, cb) => cb(null, inputFile),
-  }),
+	storage: multer.diskStorage({
+		destination: destinationFolder,
+		filename: (req, file, cb) => cb(null, inputFile),
+	}),
 });
 
 const apiRoute = mc<NextApiRequest, NextApiResponse>({
-  onError(error, req, res) {
-    res
-      .status(501)
-      .json({ error: `Sorry something Happened! ${error.message}` });
-  },
-  onNoMatch(req, res) {
-    res.status(405).json({ error: `Method '${req.method}' Not Allowed` });
-  },
+	onError(error, req, res) {
+		res.status(501).json({
+			error: `Sorry something Happened! ${error.message}`,
+		});
+	},
+	onNoMatch(req, res) {
+		res.status(405).json({ error: `Method '${req.method}' Not Allowed` });
+	},
 });
 
 const uploadMiddleware = upload.single('uploaded_file');
@@ -27,15 +27,15 @@ const uploadMiddleware = upload.single('uploaded_file');
 apiRoute.use(uploadMiddleware);
 
 apiRoute.post(async (req, res) => {
-  console.log('Sup from the api');
-  await lucidToPrisma();
-  res.status(200).json({ data: 'success' });
+	console.log('Sup from the api');
+	await lucidToPrisma();
+	res.status(200).json({ data: 'success' });
 });
 
 export default apiRoute;
 
 export const config = {
-  api: {
-    bodyParser: false, // Disallow body parsing, consume as stream
-  },
+	api: {
+		bodyParser: false, // Disallow body parsing, consume as stream
+	},
 };
